7157c5a2c5b05bd990c43aa112e101a20d140bfb
DavidM-Fox <david.mac.fox@gmail.com>
2021-02-17 13:39:21 -0600

    Major additions
    - Implemented 'stock' 'crypto' and 'exrate' commands.

--- avapi.py
+++ avapi.py
@@ -3,16 +3,26 @@ import pandas as pd
 import numpy as np
 import json
 from os import path
+from datetime import date
 from urllib.request import urlopen
-from collections import defaultdict
 
 AVAPI_KEY_JSON = "avapi_key.json"
-URL_BASE = "https://www.alphavantage.co/query?function={}&symbol={}&apikey={}"
+BASE = "https://www.alphavantage.co/query?function={}&symbol={}&apikey={}&datatype=csv"
 
-URL_FUNC = [
-    "TIME_SERIES_INTRADAY", "TIME_SERIES_DAILY", "TIME_SERIES_WEEKLY",
-    "TIME_SERIES_MONTHLY", "GLOBAL_QUOTE"
-]
+STOCK_FUNC_LIST = {
+    'intraday': "TIME_SERIES_INTRADAY",
+    'daily':    "TIME_SERIES_DAILY",
+    'weekly':   "TIME_SERIES_WEEKLY",
+    'monthly':  "TIME_SERIES_MONTHLY",
+    'global':   "GLOBAL_QUOTE"
+}
+
+CRYPTO_FUNC_LIST = {
+    'rating':   "CRYPTO_RATING",
+    'daily':    "DIGITAL_CURRENCY_DAILY",
+    'weekly':   "DIGITAL_CURRENCY_WEEKLY",
+    'monthly':  "DIGITAL_CURRENCY_MONTHLY"
+}
 
 INTERVAL_LIST = ["1min", "5min", "15min", "30min", "60min"]
 
@@ -30,8 +40,13 @@ class avapiJsonEmptyError(Exception):
     pass
 
 
-class intervalStringError(Exception):
-    """ Raised when an incorrect interval string is provided to 'intraday'"""
+class invalidFunctionError(Exception):
+    """ Raised when an incorrect function argument is provided"""
+    pass
+
+
+class invalidIntervalError(Exception):
+    """ Raised when an incorrect interval is provided to the 'intraday' argument"""
     pass
 
 
@@ -45,8 +60,10 @@ def read_key():
                     raise avapiJsonEmptyError
                 else:
                     return j['key']
+
         else:
             raise avapiJsonFileNotFoundError
+
     except avapiJsonFileNotFoundError:
         print("Error: 'avapi_key.json' file found. Try running 'setkey'.")
     except avapiJsonEmptyError:
@@ -55,33 +72,12 @@ def read_key():
         )
 
 
-def dataframe_from_av_data(url, field, object_hook=None):
-    """Returns a pandas data frame from Alpha Vantage JSON"""
-    with urlopen(url) as resource:
-        data = json.load(resource, object_hook=object_hook)[field]
-        val = {} 
-        time_data = []
-        open_data = []
-        high_data =[]
-        low_data = []
-        close_data = []
-        volume_data = []
-        
-        for timestamp, values in data.items():
-            time_data.append(timestamp)
-            open_data.append(list(values.values())[0])
-            high_data.append(list(values.values())[1])
-            low_data.append(list(values.values())[2])
-            close_data.append(list(values.values())[3])
-            volume_data.append(list(values.values())[4])
-        val['timestamp'] = time_data
-        val['open'] = open_data
-        val['high'] = high_data
-        val['low'] = low_data
-        val['close'] = close_data
-        val['volume'] = volume_data
-        df = pd.DataFrame(val)
-        return df
+def get_pandas_df(url, n):
+    if n != 0:
+        return pd.read_csv(url).head(n)
+    else:
+        return pd.read_csv(url)
+
 
 @click.group(invoke_without_command=True)  # Allow users to call w/o command
 @click.pass_context
@@ -95,8 +91,7 @@ def main(ctx, verbose):
 
     Author: Mac Fox
     """
-    if ctx.invoked_subcommand is None:
-        # No command was specified
+    if ctx.invoked_subcommand is None:  # No command was specified
         click.echo("No command specified, try using '--help'")
     ctx.obj['VERBOSE'] = verbose
 
@@ -112,30 +107,111 @@ def setkey(k):
         json.dump(avapi_key, f)
 
 
-@main.command('intraday')
-@click.option('-s', prompt="Symbol", help="Symbol: 'TSLA' 'AAPL'...")
-@click.option('-i',
-              prompt="Interval",
-              help="Data interval: '1min' '5min' '15min' '30min' or '60min'.")
+@main.command('exrate')
+@click.argument('from_currency')
+@click.argument('to_currency')
 @click.option('--save', is_flag=True, help="Save data to .csv file.")
-def intraday(s, i, save):
-    """Get intraday data for a symbol of interest.
-    
-    Example: python3 avapi.py intraday -s'TSLA' -i'15min'
+def exrate(from_currency, to_currency, save):
+    """
+    Returns the realtime exchange rate for any pair of digital currency 
+    (e.g., BTC) or physical currency (e.g., USD).
+    """
+    url = ("https://www.alphavantage.co/query?"
+           "function=CURRENCY_EXCHANGE_RATE"
+           f"&from_currency={from_currency}"
+           f"&to_currency={to_currency}"
+           f"&apikey={read_key()}")
+
+    data = json.loads(urlopen(url).read())
+    df = pd.json_normalize(data['Realtime Currency Exchange Rate'])
+    click.echo(df.to_string(index=False))
+
+    if save:
+        filename = f"{date.today()}_exrate_{f}_{t}.csv"
+        df.to_csv(filename, index=False)
+
+
+@main.command('stock')
+@click.argument('function')
+@click.option('-s', '-symbol', prompt="Symbol", help="TSLA AAPL GME...")
+@click.option('-i', '-interval', default="30min", help="1min 5min 15min 30min or 60min. Default = 30min")
+@click.option('-n', '-last_n', default=0, help="Only get last_n rows of data")
+@click.option('--save', is_flag=True, help="Save data to .csv file.")
+def stock(function, s, i, n, save):
+    """
+    Get a specific time series function for a stock of interest.
+
+    FUNCTION: intraday daily weekly monthly global
+
+    Example: python3 avapi.py stock intraday -s'TSLA' -i'15min'
+
+    Example: python3 avapi.py stock daily -s'TSLA'
+
+    Example: python3 avapi.py stock daily -s'TSLA' -n30
     """
     try:
-        if i not in INTERVAL_LIST:
-            raise intervalStringError
+        if function not in STOCK_FUNC_LIST:  # Make sure function argument is valid
+            raise invalidFunctionError
         else:
-            api_key = read_key()
-            url = URL_BASE.format(URL_FUNC[0], s,
-                                  api_key) + "&interval={}".format(i)
-            field = "Time Series ({})".format(i)
-            filename = "intraday_{}_{}".format(i, s)
-            df = dataframe_from_av_data(url, field)
-            print(df)
-    except intervalStringError:
-        click.echo("Error: Incorrect interval provided '{}'".format(i))
+            if function == "rating":
+                url = BASE.format(
+                    STOCK_FUNC_LIST["intraday"], s, read_key()) + f"&interval={i}"
+                df = get_pandas_df(url, n)
+                click.echo(df)
+
+            else:  # Construct query and get data
+                url = BASE.format(
+                    STOCK_FUNC_LIST[function], s, read_key())
+                df = get_pandas_df(url, n)
+                click.echo(df)
+
+            if save:
+                filename = f"{date.today()}_{function}_{s}_.csv"
+                df.to_csv(filename, index=False)
+
+    except invalidFunctionError:
+        click.echo(
+            "Error: Invalid function argument:'{}'".format(function))
+    except invalidIntervalError:
+        click.echo("Error: Invalid interval option:'{}'".format(i))
+
+
+@main.command('crypto')
+@click.argument('function')
+@click.option('-s', '-symbol', prompt="Symbol", help="TSLA AAPL GME...")
+@click.option('-n', '-last_n', default=0, help="Only get last_n rows of data")
+@click.option('--save', is_flag=True, help="Save data to .csv file.")
+def crypto(function, s, n, save):
+    """
+    Get a specific time series function for a crypto currency of interest.
+    """
+    try:
+        if function not in CRYPTO_FUNC_LIST:  # Make sure function argument is valid
+            raise invalidFunctionError
+        if function == 'rating':
+            url = ("https://www.alphavantage.co/query?"
+                   "function=CRYPTO_RATING"
+                   f"&symbol={s}&apikey={read_key()}")
+            data = json.loads(urlopen(url).read())
+            df = pd.json_normalize(data["Crypto Rating (FCAS)"])
+            click.echo(df.to_string(index=False))
+        else:
+            url = ("https://www.alphavantage.co/query?"
+                   f"function={CRYPTO_FUNC_LIST[function]}"
+                   f"&symbol={s}&market=USD&apikey={read_key()}"
+                   "&datatype=csv")
+            df = get_pandas_df(url, n)
+            click.echo(df)
+
+        if save:
+            filename = f"{date.today()}_{function}_{s}_.csv"
+            df.to_csv(filename, index=False)
+
+    except invalidFunctionError:
+        click.echo(
+            "Error: Invalid function argument for 'avapi get':'{}'".format(function))
+    except invalidIntervalError:
+        click.echo("Error: Invalid interval option:'{}'".format(i))
 
 
 if __name__ == "__main__":
